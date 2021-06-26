7010b6844c9584c80038f2857137ad7341a60923
JustAslaveOfGod <86428079+JustAslaveOfGod@users.noreply.github.com>
2021-06-26 11:11:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           <Route path="/questions">
             <Secondpage />
           </Route>
-          <Route exact path="/">
+          <Route exact path="/TheQuizzeto">
             <Firstpage />
           </Route>
         </Switch>
